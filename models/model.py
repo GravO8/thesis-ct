b69742a73ac1d7de8f1782c185426dac170d25d2
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-05-15 11:33:20 +0100

    Add `SiameseNet` class - closes #42

--- models/model.py
+++ models/model.py
@@ -2,13 +2,8 @@ import torch
 from abc import ABC, abstractmethod
 from final_mlp import FinalMLP
 from encoder import Encoder
-
-class Baseline3DCNN(Model):
-    def process_input(self, x):
-        return self.default_process_input(x)
-    def name_appendix(self):
-        return "baseline-3DCNN"
-
+        
+        
 class Model(ABC, torch.nn.Module):
     def __init__(self, encoder: Encoder):
         torch.nn.Module.__init__(self)
@@ -33,3 +28,21 @@ class Model(ABC, torch.nn.Module):
         
     def get_name(self):
         return self.name_appendix() + "-" + self.encoder.get_name()
+
+
+class Baseline3DCNN(Model):
+    def process_input(self, x):
+        return self.default_process_input(x)
+    def name_appendix(self):
+        return "baseline-3DCNN"
+        
+        
+class SiameseNet(Model): # expects to recieve a SiameseEncoder in the constructor
+    def process_input(self, x):
+        x           = self.default_process_input(x)
+        msp         = x.shape[2]//2             # midsagittal plane
+        hemisphere1 = x[:,:,:msp,:,:]           # shape = (B,C,x,y,z)
+        hemisphere2 = x[:,:,msp:,:,:].flip(2)   # B - batch; C - channels
+        return (hemisphere1, hemisphere2)
+    def name_appendix(self):
+        return "SiameseNet"
