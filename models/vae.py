Fix `vae_trainer.py` scan normalization

models/vae.py
@@ -54,10 +54,10 @@ def vae(name, in_channels = 1, n_start_chans = 8, dim = 3, N = 6, shape = (46, 1
     
 def vae_v1():
     return vae("vae_1", in_channels = 1, dim = 3, n_start_chans = 8, N = 6, shape = (64, 128, 128), bias = False, add_sigmoid = False)
-    
+
 def vae_v2():
     return vae("vae_v2", in_channels = 1, dim = 3, n_start_chans = 8, N = 6, shape = (64, 128, 128), bias = True, init_blocks = kaiming_init, add_sigmoid = True)
-    
+
 def vae_v3():
     return vae("vae_v3", in_channels = 1, dim = 3, n_start_chans = 16, N = 6, shape = (64, 128, 128), bias = True, init_blocks = kaiming_init, add_sigmoid = True)
     
@@ -91,7 +91,6 @@ class VAEModel(torch.nn.Module):
                     init_blocks(m)
 
     def forward(self, x):
-        x             = x / 100
         distributions = self.encoder(x)
         mu            = distributions[:, :self.z_dim]
         logvar        = distributions[:, self.z_dim:]

vae/try_vae.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     dir     = set_home(0)
     shape   = (64, 128, 128)
     loader  = HalfCTLoader(data_dir = dir, pad = shape)
-    model   = vae_v1(shape = shape, n_start_chans = 8, N = 6)
+    model   = vae_v1()
     
     path    = "../../../runs/systematic/vae/vae_v1/vae_v1-run3/weights.pt"
     model.load_state_dict(torch.load(path, map_location = torch.device("cpu")))
@@ -22,13 +22,14 @@ if __name__ == "__main__":
             pin_memory  = False)
     i = 0
     for batch in train:
-        if i < 100: 
-            i += 1
-            continue
-        x      = batch["ct"]["data"].long()
+        # if i < 100: 
+        #     i += 1
+        #     continue
+        x      = batch["ct"]["data"].float() / 100
         affine = batch["ct"]["affine"][0]
-        model.eval()
-        x_line, _, _ = model(x)
+        # model.eval()
+        # x_line, _, _ = model(x)
+        # x_line = torch.sigmoid(x_line)
         nib.save(nib.Nifti1Image(x.numpy().squeeze(), affine), "1.nii")
-        nib.save(nib.Nifti1Image(x_line.detach().numpy().squeeze(), affine), "2.nii")
+        # nib.save(nib.Nifti1Image(x_line.detach().numpy().squeeze(), affine), "2.nii")
         break

vae/vae_trainer.py
@@ -26,7 +26,7 @@ class LossTracker:
             out[l] = self.loss[l]/self.count
         return out
 
-LR          = 0.0001
+LR          = 0.001
 WD          = 0.0
 OPTIMIZER   = torch.optim.Adam
 
@@ -110,7 +110,7 @@ class VAETrainer(Trainer):
         self.writer       = SummaryWriter(run_dir)
 
     def eval_batch(self, batch):
-        x = batch["ct"][torchio.DATA].float()
+        x = batch["ct"][torchio.DATA].float() / 100
         if self.cuda:
             x = x.cuda()
         x_recon, mu, logvar = self.model(x)