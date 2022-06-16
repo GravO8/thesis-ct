bf48f85ac00f7125c07add1f9150f2089f8869a1
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-06-16 11:59:43 +0100

    Update Table data.ipynb - remove fake strokes

--- labelz.py
+++ labelz.py
@@ -3,9 +3,12 @@ import os
 import numpy as np
 
 table = pd.read_csv("../../data/gravo/table_data.csv")
+# print(len(table[(table["NCCT"] == "OK") & (table["rankin-23"] != "None")]))
+
 out = {"rankin": [], "patient_id": [], "binary_rankin": [], "set": []}
 zeors = 0
 ones = 0
+hmm = 0
 for _, row in table.iterrows():
     r = row["rankin-23"]
     if (r != "None") and r.isnumeric():
@@ -18,8 +21,11 @@ for _, row in table.iterrows():
         else:
             zeors += 1
         out["set"].append('train')
+    else:
+        hmm += 1
         
-print(zeors, ones)
+print(hmm, zeors, ones, hmm+zeors+ones)
 out = pd.DataFrame(out)
 out.to_csv("labelz.csv", index = False)
+os.system("mv labelz.csv ../../data/gravo")
         
