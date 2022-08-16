3313dd856620fd9ffeb5dd826d4be632b54d0bbf
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-08-16 17:24:18 +0100

    Update `generate_axial_features.py`

--- generate_axial_features.py
+++ generate_axial_features.py
@@ -3,18 +3,22 @@ import os, nibabel as nib, torch, tqdm
 # import matplotlib.pyplot as plt
 
 TYPE  = "CTA"
-# DIR   = "../../data/gravo"
-DIR   = "/media/avcstorage/gravo" 
-NET   = "resnet50"
+NORM  = 100 if TYPE == "NCCT" else 200
+DIR   = "../../data/gravo"
+# DIR   = "/media/avcstorage/gravo" 
+NET   = "resnet18"
 model = get_timm_model(NET, global_pool = "gap", pretrained = True, frozen = True)
 
 for file in tqdm.tqdm(os.listdir(os.path.join(DIR, TYPE))):
     if not file.endswith(".nii"):
         continue
+    path_out = os.path.join(DIR, f"{TYPE}_{NET}", f"{file.split('.')[0]}.pt")
+    if os.path.isfile(path_out):
+        continue
+    print(file)
     path = os.path.join(DIR, TYPE, file)
     scan = torch.tensor(nib.load(path).get_fdata())
     scan = scan.unsqueeze(dim = 0).permute(3,0,1,2).float()
-    scan = scan / 200
+    scan = scan / NORM
     tensor = model(scan)
-    path = os.path.join(DIR, f"{TYPE}_{NET}", f"{file.split('.')[0]}.pt")
-    torch.save(tensor, path)
+    torch.save(tensor, path_out)
