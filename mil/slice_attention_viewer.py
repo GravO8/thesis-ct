0c4ef1ede3edb030ddfdb86882b8af5aae95a644
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-08-29 11:56:49 +0100

    Update `slice_attention_viewer.py` with fancier Max Pooling "attention"

--- mil/slice_attention_viewer.py
+++ mil/slice_attention_viewer.py
@@ -1,47 +1,82 @@
 import torch, pandas as pd, nibabel as nib, os, numpy as np, matplotlib.pyplot as plt
-from tensor_mil import mil_after_attention, TensorEncoder
+from tensor_mil import mil_after_attention, TensorEncoder, mil_after_max
 
 
 def max_important_slices(argmax):
-    slices, counts = np.unique(argmax, return_counts = True)
-    sorted_i = np.argsort(counts)[::-1]
-    return slices[sorted_i], counts[sorted_i]
+    # slices, counts = np.unique(argmax, return_counts = True)
+    # sorted_i = np.argsort(counts)[::-1]
+    # return slices[sorted_i], counts
+    assert len(argmax) == len(scores)
+    slice_scores = {}
+    for i in range(len(argmax)):
+        slice = argmax[i] # the slice with highest feature i value
+        if slice in slice_scores:
+            slice_scores[slice] += scores[i]
+        else:
+            slice_scores[slice] = scores[i]
+    slice_scores = dict(sorted(slice_scores.items(), key = lambda item: item[1]))
+    a = []
+    for i in range(28):
+        if i in slice_scores:
+            a.append(slice_scores[i])
+        else: a.append(0)
+    return a
 
 
-CT_TYPE     = "CTA"
+CT_TYPE     = "NCCT"
 NET         = "resnet18"
 SET         = "test"
 DATA_DIR    = "../../../data/gravo/"
-POOOLING    = "Attetion"
-WEIGHT_PATH = f"CTA/MILNet-after-TensorAxial-{NET}_gap_2D-{POOOLING}Pooling/MILNet-after-TensorAxial-{NET}_gap_2D-{POOOLING}Pooling-run1/weights.pt"
+WEIGHT_PATH = "MILNet-after-TensorAxial-resnet18_gap_2D-MaxPooling_1Linear_features_1D/MILNet-after-TensorAxial-resnet18_gap_2D-MaxPooling_1Linear_features_1D-run1/weights.pt"
+# WEIGHT_PATH = "MILNet-after-TensorAxial-resnet18_gap_2D-AttentionPooling_1Linear_features_1D/MILNet-after-TensorAxial-resnet18_gap_2D-AttentionPooling_1Linear_features_1D-run1/weights.pt"
+POOLING    = "Attention" if "Attention" in WEIGHT_PATH else "Max"
+assert NET in WEIGHT_PATH
 
 DATASET     = os.path.join(DATA_DIR, f"dataset_{CT_TYPE}.csv")
-TENSORS_DIR = os.path.join(DATA_DIR, f"{CT_TYPE}_{NET}")
-CT_DIR      = os.path.join(DATA_DIR, CT_TYPE)
-SKIP_SLICES = 5
+CT_DIR      = os.path.join(DATA_DIR, f"{CT_TYPE}_normalized")
+TENSORS_DIR = os.path.join(CT_DIR, f"{CT_TYPE}_{NET}")
+SKIP_SLICES = 2
 N           = 5
+CLIP        = True
 
-
-model = mil_after_attention(TensorEncoder(NET, 512, "gap"))
-model.load_state_dict(torch.load(WEIGHT_PATH))
+if POOLING == "Attention":
+    model = mil_after_attention(TensorEncoder(NET, 512, "gap"))
+elif POOLING == "Max":
+    model = mil_after_max(TensorEncoder(NET, 512, "gap"))
+weights = torch.load(WEIGHT_PATH, map_location = torch.device("cpu"))
+model.load_state_dict(weights)
 model.eval()
 set  = pd.read_csv(DATASET)
 set  = set[set["set"] == SET]
+
+if POOLING == "Max":
+    encoder      = weights["encoder.feature_extractor.encoder.0.weight"].squeeze().numpy().T
+    mlp          = weights["mlp.0.weight"].squeeze().numpy()
+    encoder[encoder < 0] = 0
+    mlp = np.abs(mlp)
+    scores   = encoder.dot(mlp)
+    
+    
 
 for _, row in set.iterrows():
     patient = row["patient_id"]
     y_true  = row["binary_rankin"]
     tensor  = torch.load(os.path.join(TENSORS_DIR, f"{patient}.pt"))
     scan    = nib.load(os.path.join(CT_DIR, f"{patient}.nii")).get_fdata()
+    if CLIP:
+        tensor = tensor[15:70,:]
+        scan   = scan[:,:,15:70]
     i_range = range(0,len(tensor),SKIP_SLICES)
     tensor  = tensor[i_range]
     scan    = scan[...,i_range]
     pred, a = model.predict_attention(tensor)
     a       = a.detach().numpy().squeeze()
-    if POOOLING == "Attetion":
-        i_att = np.argsort(a)[::-1]
-    elif POOOLING == "Max":
-        i_att, a = max_important_slices(a)
+    if POOLING == "Max":
+        a = max_important_slices(a)
+    # if POOLING == "Attetion":
+    i_att = np.argsort(a)[::-1]
+    # elif POOLING == "Max":
+    #     i_att, a = max_important_slices(a)
     print("patient:", patient)
     print(" y_true:", y_true)
     print(" y_pred:", pred)
