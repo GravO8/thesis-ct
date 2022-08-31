04c8647d96762a6de0ec3848d27e1582dbdbf940
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-08-31 11:30:34 +0100

    Update `main.py` to support the single train/test split too

--- utils/main.py
+++ utils/main.py
@@ -11,26 +11,35 @@ def set_home(device: int):
     return dir
     
     
-def train(to_test: list, ct_loader, trainer):
-    for model in to_test:
-        for train, test in ct_loader.get_folds():
-            trainer.train(model, train, test, ct_loader.skip_slices)
+def train(to_test: list, ct_loader, trainer, N: int = None):
+    if ct_loader.kfold:
+        for model in to_test:
+            for train, test in ct_loader.get_folds():
+                trainer.train(model, train, test, ct_loader.skip_slices)
+    else:
+        assert N is not None
+        for model in to_test:
+            for _ in range(N):
+                ct_loader.reshuffle()
+                train, test = ct_loader.load_dataset()
+                trainer.train(model, train, test, ct_loader.skip_slices)
     print("done")
 
 
 def main(to_test: list, device: int = 3, slice: str = None, 
-    slice_range: int = None, pad: int = None, from_tensors = False, **kwargs):
+    slice_range: int = None, pad: int = None, from_tensors = False, N: int = None, 
+    kfold = False, **kwargs):
     dir = set_home(device)
     if from_tensors:
-        ct_loader = CTLoaderTensors(data_dir = dir, kfold = True, **kwargs)
+        ct_loader = CTLoaderTensors(data_dir = dir, kfold = kfold, **kwargs)
     elif slice_range is not None:
         assert slice is not None
-        ct_loader = CTLoader2D(slice, kfold = True, slice_range = slice_range, data_dir = dir, 
+        ct_loader = CTLoader2D(slice, kfold = kfold, slice_range = slice_range, data_dir = dir, 
                     pad = pad, **kwargs)
         for model in to_test:
             model.set_slice_info(slice_range, slice)
     else:
-        ct_loader = CTLoader(data_dir = dir, kfold = True, **kwargs)
+        ct_loader = CTLoader(data_dir = dir, kfold = kfold, **kwargs)
     trainer = Trainer(batch_size = 32)
-    train(to_test, ct_loader, trainer)
+    train(to_test, ct_loader, trainer, N)
     
