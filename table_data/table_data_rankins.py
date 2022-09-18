63ed95f31648cea66078e333c252680d6640258b
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-09-18 19:40:26 +0100

    Add LR tests with only important features

--- table_data/table_data_rankins.py
+++ table_data/table_data_rankins.py
@@ -22,8 +22,11 @@ from stages import *
 for stage in STAGES:
     for missing in ("amputate", "impute", "impute_mean", "impute_constant"):
         print(stage, missing)
+        # stage,missing_values,model,set,f1_score,accuracy,precision,recall,auc
         loader  = TableLoader("table_data.csv",
-                            keep_cols           = stage,
+                            keep_cols           = ["altura-1", "peso-1", "age", "hemoAd-4", "hemat-4",
+                                                   "inrAd-4", "gliceAd-4", "totalNIHSS-5", "preArtSis-5", 
+                                                   "preArtDia-5"],
                             target_col          = "binary_rankin",
                             normalize           = True,
                             dirname             = "../../../data/gravo",
@@ -34,6 +37,7 @@ for stage in STAGES:
                             filter_out_no_ncct  = False,
                             empty_values_method = missing)
         print()
-        for classifier in (knn, decision_tree, random_forest, logistic_regression, gradient_boosting):
+        for classifier in [logistic_regression]:
+         # (knn, decision_tree, random_forest, logistic_regression, gradient_boosting):
             trained_classifier = classifier(loader, n_iter = 50, metric = "f1")
             trained_classifier.record_performance(stage, missing)
